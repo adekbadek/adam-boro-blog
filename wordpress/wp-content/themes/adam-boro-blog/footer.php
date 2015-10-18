f8c33b170745f5791c033e2190267b6e37ab61d9
Adam Borowski <adam@adamboro.com>
2015-10-18 18:24:56 +0200

    Underscores cleanup

--- wordpress/wp-content/themes/adam-boro-blog/footer.php
+++ wordpress/wp-content/themes/adam-boro-blog/footer.php
@@ -15,9 +15,6 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'adam-boro-blog' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'adam-boro-blog' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'adam-boro-blog' ), 'adam-boro-blog', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
