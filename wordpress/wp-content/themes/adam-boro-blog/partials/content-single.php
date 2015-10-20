f7d947048fb10ce147a563135f83f2a4eade0f90
Adam Borowski <adam@adamboro.com>
2015-10-21 01:07:49 +0200

    Disqus comments

--- wordpress/wp-content/themes/adam-boro-blog/partials/content-single.php
+++ wordpress/wp-content/themes/adam-boro-blog/partials/content-single.php
@@ -1,16 +1,17 @@
 <div class="single-wrapper">
 	<div class="single">
-	<h2><?php the_title(); ?></h2>
-	<div class="single-data">
-		<p class="category smaller"><?php
-			foreach((get_the_category()) as $category) {
-				echo '<a href="'. home_url( '/' ) . $category->slug .'">' . $category->cat_name . '</a> ';
-			}?>
-		</p>
-		<p class="time dimmed smaller"><?php the_time( 'F jS, Y' ); ?></p>
-	</div>
-	<div class="single-content">
-		<?php the_content(); ?>
-	</div>
+		<h2><?php the_title(); ?></h2>
+		<div class="single-data">
+			<p class="category smaller"><?php
+				foreach((get_the_category()) as $category) {
+					echo '<a href="'. home_url( '/' ) . $category->slug .'">' . $category->cat_name . '</a> ';
+				}?>
+			</p>
+			<p class="time dimmed smaller"><?php the_time( 'F jS, Y' ); ?></p>
+		</div>
+		<div class="single-content">
+			<?php the_content(); ?>
+		</div>
+		<?php comments_template(); ?>
 	</div>
 </div>
