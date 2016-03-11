375548fa3a7169dceb9cb99ea5ea86bdff9d3ce4
Adam Borowski <adam@adamboro.com>
2016-03-11 21:16:25 +0100

    Reorganize styles, delete script contents

--- wordpress/wp-content/themes/adam-boro-blog/header.php
+++ wordpress/wp-content/themes/adam-boro-blog/header.php
@@ -19,53 +19,16 @@
 <?php wp_head(); ?>
 </head>
 
-<?php
-	// READ COOKIES AND ASSIGN BODY CLASSES
-	//
-	// BRIGHTNESS (BRIGHT OR DARK)
-	// bright by default:
-	// read cookie
-	// if (isset($_COOKIE['bright_class'])) {
-	// 	$body_bright_class = $_COOKIE['bright_class'];
-	// }else{
-	// 	// set default cookie val
-	// 	$body_bright_class = 'bright';
-	// 	// setcookie('bright_class', $body_bright_class, strtotime('+10 year'));
-	// };
-	//
-	// WIDTH (HEADER HIDDEN OR SHOWN)
-	// menu shown by default ('' or 'flip'):
-	// read cookie
-	if (isset($_COOKIE['width_class'])) {
-		$body_width_class = $_COOKIE['width_class'];
-	}else{
-		// set default cookie val
-		$body_width_class = 'with_menu';
-		// setcookie('width_class', $body_width_class, strtotime('+10 year'));
-	};
-
-	// echo 'width class: ' . $body_width_class . '<br>';
-	// echo 'bright class: ' . $body_bright_class;
-?>
 <body <?php body_class( $body_width_class ); ?>>
 
 <div id="page" class="hfeed site">
 
 	<header id="masthead" class="site-header" role="banner">
 
-		<div id="flipper-arrow" class="flip-wrapper">
-			<div class="flip-cont flip">
-				<div class="line flippable bottom"></div>
-				<div class="line flippable left"></div>
-				<div class="line center"></div>
-			</div>
-		</div>
-
 		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="notlink"><?php bloginfo( 'name' ); ?></a></h1>
 			<p class="site-description">
 				This site is where I keep track of the stuff I learn as a side effect of coding.
-				<!-- If it's too <span id="bright-click-section">bright</span> <span id="bright-click" class="span-link">click here</span>. -->
 				<br>
 				<br class="divider">
 				I also <a href="http://adamboro.com/links/" target="_blank">collect links</a> and am <a href="mailto:adam@adamboro.com&subject=I have work for you.">available for work</a>. Check out <a href="http://adamboro.com/" target="_blank">my portfolio</a>.
