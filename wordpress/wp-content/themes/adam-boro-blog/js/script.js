9fd405dd83b9c874d7a13b6c22474dff47ddbbd6
Adam Borowski <adam@adamboro.com>
2016-04-09 20:33:01 +0200

    add GH embedded gist expanding

--- wordpress/wp-content/themes/adam-boro-blog/js/script.js
+++ wordpress/wp-content/themes/adam-boro-blog/js/script.js
@@ -1,1 +1,9 @@
-console.log('hello');
+var gists = document.getElementsByClassName('gist')
+for (var i = 0; i < gists.length; i++) {
+  gists[i].style.height = gists[i].offsetHeight + 'px'
+  gists[i].classList.add('g-expandable')
+  gists[i].classList.add('g-hidden')
+  gists[i].onclick = function(e){
+    e.target.classList.remove('g-hidden')
+  }
+}
